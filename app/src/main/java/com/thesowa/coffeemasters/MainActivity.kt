cacd4029052ce60541155c764117e87163be335b
Paweł Sowa <pawel.sowa@codequest.com>
2022-07-15 14:08:05 +0200

    feat: text field

--- app/src/main/java/com/thesowa/coffeemasters/MainActivity.kt
+++ app/src/main/java/com/thesowa/coffeemasters/MainActivity.kt
@@ -3,13 +3,22 @@ package com.thesowa.coffeemasters
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.thesowa.coffeemasters.ui.theme.CoffeeMastersTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +28,7 @@ class MainActivity : ComponentActivity() {
             CoffeeMastersTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Text("Android")
+                    FirstComposable()
                 }
             }
         }
@@ -29,11 +38,17 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun FirstComposable() {
-    Text(text = "Hello Jetpack Compose")
-}
+    var name = remember {
+        mutableStateOf("")
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun SecondComposable() {
-    Text(text = "Second Compose")
+    Column() {
+        Text("Hello ${name.value}", modifier = Modifier
+            .padding(16.dp)
+            .background(Color.Yellow)
+            .padding(16.dp))
+        TextField(value = name.value, onValueChange = {
+            name.value = it
+        })
+    }
 }
